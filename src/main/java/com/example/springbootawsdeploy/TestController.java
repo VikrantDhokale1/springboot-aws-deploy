cdf83da3f45141d1fc5eed00585e08e0bdb6f5b7
VikrantDhokale1 <139742612+VikrantDhokale1@users.noreply.github.com>
2024-06-18 19:28:18 +0530

    Update TestController.java

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/data")
     public String getData() {
-        return "First message from  AWS    deploy";
+        return "First message from  AWS    deploy1";
     }
 
     @GetMapping("/message")
